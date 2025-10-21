b55b9f96a840dfde85f857b6a0644fe20a86e0ce
Sen-Vmh <r0982669@student.thomasmore.be>
2025-10-22 01:00:54 +0200

    added working grid implementation and restructured working directory

---
+++ src/app/components/layout/app/diagram-panel/Grid.jsx
@@ -0,0 +1,73 @@
+
+import { Background, Controls, MiniMap, Panel, ReactFlow, addEdge, applyEdgeChanges, applyNodeChanges } from "@xyflow/react";
+import '@xyflow/react/dist/style.css';
+import { useCallback, useState } from "react";
+import Premise from "../../../ui/diagram/nodes/Premise";
+import Argument from "app/components/ui/diagram/nodes/Argument";
+
+const Grid = ({
+  title = "Free Will."
+}) => {
+
+  const nodeTypes = {
+    premise: Premise,
+    argument: Argument
+  };
+  
+  const initialNodes = [
+    {
+      id: 'n1', // id needs to match that of the conversation
+      position: { x: 0, y: 0 }, // don't worry about this value, I will compute it programmatically
+      type: "premise", // what category the node falls under; for now the options should be: premise, argument, counterargument
+      data: { label: "Free will does not exist.", person: "primary"}, // person being either primary or secondary. Essentially a boolean value
+    },
+    {
+      id: 'n2',
+      position: { x: 100, y: 100 },
+      type: "argument",
+      data: { label: "Everything we do is based on what we want, but we don't decide what we want.", person: "secondary"},
+    },
+  ]
+
+  const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }]; // the program handles the edge connections like this. Each connection has an object: {} with id, source, and target.
+  // you can either embed this data alongside initial nodes or have it separate; having it separate would be best as it would minimize programming requirements.
+
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
+  return (
+    <div className="p-3 w-full h-full flex flex-col gap-3 items-center justify-center">
+      <ReactFlow 
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Background></Background>
+        <Controls showInteractive={true} />
+        <Panel position="top-left">{title}</Panel>
+
+      </ReactFlow>
+
+      {/* <Analysis></Analysis> */}
+    </div>
+  );
+};
+
+export default Grid;
---
+++ src/app/components/ui/diagram/nodes/Premise.jsx
@@ -0,0 +1,28 @@
+import { Handle, Position } from "@xyflow/react";
+
+function Premise({ 
+        data
+    }) {
+
+    const styles = {
+        primary: "border-blue-700 bg-blue-100",
+        secondary: "border-green-700 bg-green-100",
+    }
+    
+    const titleStyles = {
+        primary: "text-blue-900",
+        secondary: "text-green-900",
+    }
+
+    return (
+        <>
+            <div className={`${styles[data.person]} w-60 p-3 text-center flex items-center justify-center border-2 rounded-full`}>
+                <h2 className={`${titleStyles[data.person]} text-xl font-primary uppercase font-bold`}>{data.label}</h2>
+                <Handle type="source" position={Position.Bottom} />
+                <Handle type="target" position={Position.Top} />
+            </div>
+        </>
+    );
+}
+
+export default Premise;
